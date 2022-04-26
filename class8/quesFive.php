21d0005f6c5432391a810abe81ed793060164646
unknown <fuadhasanemon8@gmail.com>
2022-04-26 12:59:14 +0600

    Assignment done

--- class8/quesFive.php
+++ class8/quesFive.php
@@ -11,6 +11,27 @@
     <?php 
 
         // Create a BMI function for health
+        // A BMI of 25.0 or more is overweight, while the healthy range is 18.5 to 24.9
+
+        // $bmi = 0;
+
+        function bmiCalculator($weight, $height) {
+            $bmi = $weight / pow($height, 2) ;
+            $bmi = round($bmi, 2);
+            
+            if ($bmi <= 18.5){
+                echo "Your Body Mass Index is {$bmi} . This is considered severely underweight." ;
+            } else if ($bmi > 18.5 AND $bmi <= 24.9) {
+                echo "Your Body Mass Index is {$bmi} . This is considered Normal." ;
+            } else if ($bmi > 25) {
+                echo "Your Body Mass Index is {$bmi} . This is considered Overweight." ;
+            }
+            
+        }
+
+       
+
+        bmiCalculator(90, 6);
 
         
 
--- class8/quesThree.php
+++ class8/quesThree.php
@@ -12,6 +12,28 @@
 
         // GPA function for result publishing
 
+        function resultPublishing($SubjectA, $SubjectB, $SubjectC, $SubjectD) {
+            $totalMark = $SubjectA + $SubjectB + $SubjectC + $SubjectD;
+            $rishitasMark = $totalMark / 4;
+
+            if ($rishitasMark <= 100 && $rishitasMark >= 80){
+                echo "You got A+";
+            } elseif($rishitasMark >= 70 && $rishitasMark < 80 ) {
+                echo "You got A";
+            } elseif($rishitasMark >= 60 && $rishitasMark < 70 ) {
+                echo "You got A-";
+            } elseif($rishitasMark >= 50 && $rishitasMark < 60 ) {
+                echo "You got B";
+            } elseif($rishitasMark >= 40 && $rishitasMark < 50 ) {
+                echo "You got C";
+            } else {
+                echo "You got F";
+            }
+        }
+
+        resultPublishing(80, 90, 88, 90);
+
+        
     ?>
     
 </body>
