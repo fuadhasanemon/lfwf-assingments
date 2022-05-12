a987e6d15d9df1f8b01dede0ba4e276a49e5f390
unknown <fuadhasanemon8@gmail.com>
2022-05-12 18:50:38 +0600

    Class ten assignment

--- class_10/form_validate.php
+++ class_10/form_validate.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,35 +8,79 @@
     <title>Document</title>
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
     <!-- Onpage php -->
 
     <?php
-        if(isset($_POST['submitBtn'])) {
-            $userName = $_POST['userName'];
-            $email = $_POST['email'];
-            $phone = $_POST['phone'];
-            $pasword =  $_POST['pasword'];
-            
-            
-            if (empty($userName) || empty($email) || empty($phone) || empty($pasword)) {
-                echo '<script> alert("All field are required"); </script>';
-            } else {
-                echo "thank you submission successfull";
-            }
+
+    /**
+     * Validate message
+     * custome function
+     */
+
+    function validate($message, $alertType="danger"){
+      return "<div class=\"alert alert-{$alertType} alert-dismissible fade show\" role=\"alert\">
+                    {$message}!
+                <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
+                    <span aria-hidden=\"true\">&times;</span>
+                </button>
+            </div>";
+    }
+
+
+    /**
+     * 
+     * Validate email valid or not!
+     * 
+     */
+
+
+     function validateEmail($email){
+         if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            return true;
+         } else {
+             return false;
+         }
+     }
+
+
+    if (isset($_POST['submitBtn'])) {
+        $userName = $_POST['userName'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $pasword =  $_POST['pasword'];
+
+
+        if (empty($userName) || empty($email) || empty($phone) || empty($pasword)) {
+            $validationMsg = validate('All field are required');
+        } elseif(filter_var($email, FILTER_VALIDATE_EMAIL) == false){
+            $validationMsg = validate('Email is not valid', 'warning');
+        } else {
+            $validationMsg = validate('Thank you submission successfull', 'success');
         }
+    }
     ?>
 
     <!-- Onpage php -->
-    
+
 
     <div class="section form">
         <div class="container">
-            <div class="row">
+
+            <?php
+                if(isset($validationMsg)){
+                    echo $validationMsg;
+                }
+
+            ?>
+
+            <div class="form__wrapper">
+
                 <form method="POST" action="">
 
                     <div class="form-group">
@@ -58,18 +103,26 @@
                         <label for="exampleInputPassword1">Password</label>
                         <input type="password" name="pasword" class="form-control" id="exampleInputPassword1" placeholder="Password">
                     </div>
-                    
+
                     <button type="submit" name="submitBtn" class="btn btn-primary">Submit</button>
                 </form>
+
             </div>
+
         </div>
     </div>
 
 
+
+
     <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 
     <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
