6f4ec4a1dad3d15fa4b64b136c027e42a3b0d7e9
fuadhasanemon <fuadhasanemon8@gmail.com>
2022-06-02 11:37:36 +0600

    some modification

--- class_14/all_queries.sql
+++ class_14/all_queries.sql
@@ -1,30 +1,30 @@
-*** Create a Database name  user_information.
+-- *** Create a Database name  user_information.
 
-*** In Users table 7 column:  id  , username , age , phone, address, salary , country  ,created_at, updated_at
+-- *** In Users table 7 column:  id  , username , age , phone, address, salary , country  ,created_at, updated_at
 
-*** Find users who live in same country 
+-- *** Find users who live in same country 
 
-*** Show data  by high to low salary 
+-- *** Show data  by high to low salary 
 
-*** Show data who get the highest salary
+-- *** Show data who get the highest salary
 
-*** Show data who get the lowest 
+-- *** Show data who get the lowest 
 
-*** Show how many table you have 
+-- *** Show how many table you have 
 
-*** Show me how many users get a salary range of ( 5000 -15000).
+-- *** Show me how many users get a salary range of ( 5000 -15000).
 
 
 
 -- Ques 1
 
-******** For Create Database ********
+-- ******** For Create Database ********
 
 CREATE DATABASE user_information;
 
 -- Ques 2
 
-******** For Create table for users ********
+-- ******** For Create table for users ********
 
 CREATE TABLE Users (
     id INT NOT NULL PRIMARY KEY,
@@ -41,7 +41,7 @@ CREATE TABLE Users (
 
 -- Ques 3
 
-******** For find users who live in same country  ********
+-- ******** For find users who live in same country  ********
 
 -- Users table
 
@@ -50,7 +50,7 @@ SELECT country FROM users GROUP BY country;
 
 -- Ques 4
 
-******** Show data  by high to low salary ********
+-- ******** Show data  by high to low salary ********
 
 -- Users table
 
@@ -58,7 +58,7 @@ SELECT * FROM users ORDER BY salary DESC;
 
 -- Ques 5
 
-******** Show data who get the highest salary ********
+-- ******** Show data who get the highest salary ********
 
 -- Users table
 
@@ -67,7 +67,7 @@ SELECT * FROM users where salary=(select Max(salary) from users);
 
 -- Ques 6
 
-******** Show data who get the lowest  ********
+-- ******** Show data who get the lowest  ********
 
 -- Users table
 
@@ -75,7 +75,7 @@ SELECT * FROM users where salary=(select Min(salary) from users);
 
 -- Ques 7
 
-******** Show how many table you have ********
+-- ******** Show how many table you have ********
 
 SELECT COUNT(*) from INFORMATION_SCHEMA.TABLES
 WHERE table_schema = 'user_information';
@@ -83,6 +83,6 @@ WHERE table_schema = 'user_information';
 
 -- Ques 8
 
-******** Show me how many users get a salary range of ( 5000 - 5000) ********
+-- ******** Show me how many users get a salary range of ( 5000 - 5000) ********
 
 SELECT * from users WHERE salary BETWEEN 5000 AND 5000;
